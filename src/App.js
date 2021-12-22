1593688b715f02c7ca0ed0d200a8879cae08de49
solenedel <solene98@student.ubc.ca>
2021-12-22 15:36:29 -0800

    move timer logic to Timer component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
 import Form from "./components/Form";
 import { GlobalStyles } from "./components/styled/GlobalStyles.style";
-// import Timer from "./components/Timer";
+import Timer from "./components/Timer";
+
 
 // unique local storage key to store the todos
 // const LOCAL_STORAGE_KEY_NUMBERS = 'numbersFromUser';
@@ -33,20 +34,6 @@ function App() {
      return () => clearInterval(interval); 
    }, [timerOn]);
 
-   /*
-
-   // persist numbers state on reload
-   useEffect(() => {
-     const storageNumbers = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY_NUMBERS));
-     if (storageNumbers) setNumbers(storageNumbers);
-   }, []);
-
-   // save numbers to local storage
-  useEffect(() => {
-    localStorage.setItem(LOCAL_STORAGE_KEY_NUMBERS, JSON.stringify(numbers));
-  }, [numbers]);
-
-  */
 
   return (
     <div className="App">
@@ -63,24 +50,7 @@ function App() {
         </ul>
       </div>
       <div>
-        <span id="minutes">{('0' + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
-        <span id="seconds">{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span> 
-      </div>
-      <div className="buttons">
-        
-        {!timerOn && time === 0 && (
-          <button onClick={() => setTimerOn(true)}>Start</button>)}
-        {timerOn && (<button onClick={() => setTimerOn(false)}>Stop</button>)}  
-        {!timerOn && time !== 0 && (
-          <button onClick={() => setTimerOn(true)}>Resume</button>
-        )}
-        {!timerOn && time > 0 && ( 
-           <button onClick={() => {
-          setTimerOn(false);
-          setTime(0);
-        }}>Reset</button>
-        )}
-       
+      <Timer time={time} setTime={setTime} timerOn={timerOn} setTimerOn={setTimerOn} />
       </div>
     </div>
   );
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,12 +1,28 @@
 import React from "react";
 
-const Timer = ({ minutes, setMinutes, seconds, setSeconds}) => {
+const Timer = ({ time, setTime, timerOn, setTimerOn }) => {
 
   return ( 
     <div className="timer">
       <h3>timer</h3>
-      <div>{minutes} : {seconds}</div>
-      <button>Start</button>
+      <span id="minutes">{('0' + Math.floor((time / 60000) % 60)).slice(-2)} : </span>
+      <span id="seconds">{('0' + Math.floor((time / 1000) % 60)).slice(-2)}</span> 
+      <div className="buttons">
+        
+        {!timerOn && time === 0 && (
+          <button onClick={() => setTimerOn(true)}>Start</button>)}
+        {timerOn && (<button onClick={() => setTimerOn(false)}>Stop</button>)}  
+        {!timerOn && time !== 0 && (
+          <button onClick={() => setTimerOn(true)}>Resume</button>
+        )}
+        {!timerOn && time > 0 && ( 
+           <button onClick={() => {
+          setTimerOn(false);
+          setTime(0);
+        }}>Reset</button>
+        )}
+       
+      </div>
     </div>
    );
 }
