9acdbe945f6122f9532e05210595ca12fdf4c10d
solenedel <solene98@student.ubc.ca>
2021-12-21 22:10:23 -0800

    implementing timer

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,11 +1,28 @@
 import React from "react";
 
 const Timer = ({ minutes, setMinutes, seconds, setSeconds}) => {
+
+  const startTimer = () => {
+    console.log('timer started!');
+
+    const intervalFunc = () => {
+      setMinutes(minutes - 1);
+      console.log('MINUTES ', minutes);
+
+       if (minutes === 0) {
+      console.log('minutes has reached zero');
+    }
+    };
+
+    const intervalId = setInterval(intervalFunc, 1000);
+    clearInterval(intervalId);
+  };
+
   return ( 
     <div className="timer">
       <h3>timer</h3>
       <div>{minutes} : {seconds}</div>
-      <button>Start</button>
+      <button onClick={() => startTimer()}>Start</button>
     </div>
    );
 }
